a9e2445930d73eee17c013cd7cde02775def7dc6
Cerry0524 <Cerry0524@gmail.com>
2023-05-26 00:22:49 +0800

    add收支表格式完成，調整陣列POST未完成

--- api/add_summary.php
+++ api/add_summary.php
@@ -1,30 +1,34 @@
 <?php
 include_once "../db.php";
 
-    $sql = "INSERT INTO `summary`( `project`, `details`, `amount`,`effective_time`,`private`, `continuous`)
-                          VALUES ('{$_POST['project']}', '{$_POST['details']}', '{$_POST['amount']}', '{$_POST['eff_time']}', '{$_POST['private']}', '{$_POST['continuous']}')";
-   
-    $pdo->exec($sql);//新增收支項目
+print_r($_POST['project']);
+
+for ($i=0; $i <count($_POST['project']); $i++) { 
     
-    $sql_category_id = "select `id` from `categories` where `category`='{$_POST['category']}'";//查詢categories是否存在資料庫內
-    $category_id = $pdo->query($sql_category_id)->fetchColumn();//回傳id給$category_id
+    $sql = "INSERT INTO `summary`( `project`, `details`, `amount`,`price`,`effective_time`,`private`, `continuous`)
+                          VALUES ('{$_POST['project'][$i]}', '{$_POST['details'][$i]}', '{$_POST['amount'][$i]}','{$_POST['price'][$i]}', '{$_POST['eff_time'][$i]}', '{$_POST['private'][$i][$i]}', '{$_POST['continuous'][$i][$i]}')";
+   
+    $pdo->exec($sql);
+    
+    $sql_category_id = "select `id` from `categories` where `category`='{$_POST['category'][$i]}'";
+    $category_id = $pdo->query($sql_category_id)->fetchColumn();
 
     print_r($category_id);
 
     if($category_id>0){
 
     }else{
-        $sql="INSERT INTO `categories`(`category`) VALUES ('{$_POST['category']}')";//新增類別進資料庫
-        $pdo->exec($sql);//指令送資料庫
-        $sql_category_id = "select `id` from `categories` where `category`='{$_POST['category']}'";//查詢categories是否存在資料庫內
-        $category_id = $pdo->query($sql_category_id)->fetchColumn();//回傳id給$category_id
-    };
+        $sql="INSERT INTO `categories`(`category`) VALUES ('{$_POST['category'][$i]}')";
+        $pdo->exec($sql);
+        $sql_category_id = "select `id` from `categories` where `category`='{$_POST['category'][$i]}'";
+        $category_id = $pdo->query($sql_category_id)->fetchColumn();
+    }
    
-    $sql_category_update="UPDATE `summary` SET `category_id`=$category_id WHERE `project`='{$_POST['project']}'";//更新新建收支表的category_id
-    $pdo->exec($sql_category_update);//指令送資料庫
-    
-    $sql_inerstNew="SELECT `summary`.`project`,`categories`.`category`,`summary`.`details`,`summary`.`amount`,`summary`.`effective_time`, `summary`.`private`, `summary`.`continuous` 
-                    FROM `summary`,`categories`
-                    WHERE `summary`.`category_id`=`categories`.id`";
+    $sql_category_update="UPDATE `summary` SET `category_id`=$category_id WHERE `project`='{$_POST['project'][$i]}'";
+    $pdo->exec($sql_category_update);
+}    
 
-header("location:../index.php?do=summary_list");
+$sql_inerstNew="SELECT `summary`.`project`,`categories`.`category`,`summary`.`details`,`summary`.`amount`,`summary`.`price`,`summary`.`effective_time`, `summary`.`private`, `summary`.`continuous` 
+                FROM `summary`,`categories`
+                WHERE `summary`.`category_id`=`categories`.`id`";
+?>
--- front/add_summary.php
+++ front/add_summary.php
@@ -1,74 +1,95 @@
 <link rel="stylesheet" href="../css/style_addsummary.css">
 <script src="../js/jquery-3.7.0.min.js"></script> <!-- 引進jquery -->
 
-<div class="content">
-    <div class="content-top">
-        <div name="incomeBtn" id="incomeBtn" value="0">收入</div>
-        <div name="expendBtn" id="expendBtn" value="1">支出</div>
-    </div>
-    <div class="content-middle">
-        <div class="content-middle-left"></div>
-        <div class="content-middle-center">
-            <form action="./api/add_summary.php" method="post">
-                <div class="add-table">
-                    <div>
-                        <div class="th"><label for="eff_time">發生日期</label></div>
-                        <div class="th"><label for="project">項目</label></div>
-                        <div class="th"><label for="details">細節</label></div>
-                        <div class="th"><label for="category">類別</label></div>
-                        <div class="th"><label for="amount">數量</label></div>
-                        <div class="th"><label for="pravite">備註</label></div>
-                    </div>
-                    <div class="options">
-                        <div>
-                            <div class="tr"><input type="date" name="eff_time[]" id="eff_time"></div>
-                            <div class="tr"><input type="text" name="project[]" id="project"></div>
-                            <div class="tr"><input type="text" name="details[]" id="details"></div>
-                            <div class="tr"><input style="max-width: 100px;min-width:50px" type="text" name="category" id="category"></div>
-                            <div class="tr"><input style="max-width: 60px;" type="number" name="amount" id="amount"></div>
-                            <div class="tr" style="min-width: 160pxdiv>
-                                <input type=" radio" name="private[]" value="0" checked>公開
-                                <input type="radio" name="private[]" value="1">不公開<br>
-                                <input type="radio" name="continuous[]" value="0" checked>無持續
-                                <input type="radio" name="continuous[]" value="1">有持續
-                            </div>
-                        </div>
-                        <span onclick="addOption()">+</span>
-                        <!-- jquery建立一個function 增加項目 -->
-                        <span onclick="removeOption(this)">-</span>
-                        <!-- jquery建立一個function 減少項目 ()裡面要加上this表示這個div-->
-                    </div>
 
-                </div>
-        </div>
-        <button type="submit">確認</button>
-        </form>
-    </div>
-    <div class="content-middle-left"></div>
-</div>
-<div class="content-footer"></div>
-</div>
+<form action="./api/add_summary.php" method="post">
+
+
+
+    <div class="content-left"></div>
+    <table class="add-table">
+        <tr class="content-top">
+            <td name="incomeBtn" id="incomeBtn" value="0">收入</td>
+            <td name="expendBtn" id="expendBtn" value="1">支出</td>
+        </tr>
+        <tr class="add-table-tr1">
+            <th style="width: 5%;"></th>
+            <th class="add-table-th1"><label for="eff_time">發生日期</label></th>
+            <th class="add-table-th2"><label for="project">項目</label></th>
+            <th class="add-table-th3"><label for="details">細節</label></th>
+            <th class="add-table-th4"><label for="category">類別</label></th>
+            <th class="add-table-th5"><label for="amount">數量</label></th>
+            <th class="add-table-th6"><label for="price">金額</label></th>
+            <th class="add-table-th7"><label for="pravite">備註</label></th>
+            <th class="add-table-th8">增減項目</th>
+            <th style="width: 5%;"></th>
+        </tr>
+        <tr class=tdInput style='background-color: rgb(210, 222,239);height: 40px;border-bottom: 1px solid dodgerblue; '>
+            <th style="width: 5%;"></th>
+            <td><input type="date" name="eff_time[]" id="eff_time"></td>
+            <td><input type="text" name="project[]" id="project"></td>
+            <td><input type="text" name="details[]" id="details"></td>
+            <td><input type="text" name="category[]" id="category"></td>
+            <td><input type="number" name="amount[]" id="amount" min="0"></td>
+            <td><input type="number" name="price[]" id="price" min="0"></td>
+            <td>
+                <input type="radio" name="private[${optionIndex}][]" value="0" checked>公開
+                <input type="radio" name="private[${optionIndex}][]" value="1">不公開<br>
+                <input type="radio" name="continuous[${optionIndex}][]" value="0" checked>無持續
+                <input type="radio" name="continuous[${optionIndex}][]" value="1">有持續
+            </td>
+            <td>
+                <span onclick="addOption(this)" style="font-size: 26px;">＋</span><br>
+                <span onclick="removeOption(this) " style="font-size: 26px;">－</span>
+            </td>
+            <th style="width: 5%;"></th>
+        </tr>
+        <tr class=lastInput>
+            <th style="width: 5%;" colspan="10"></th>
+        </tr>
+    </table>
+    <br>
+    <button type="submit" class="addBtn">確認</button>
+</form>
+
+
+
 
 <script>
-    function addOption() {
-        let opt = `<div>
-                        <div><input type="date" name="eff_time[]" id="eff_time"></div>
-                        <div><input type="text" name="project[]" id="project"></div>
-                        <div><input type="text" name="details[]" id="details"></div>
-                        <div><input style="max-width: 100px;min-width:50px" type="text" name="category" id="category"></div>
-                        <div><input style="max-width: 60px;" type="number" name="amount" id="amount"></div>
-                        <div style="min-width: 160pxdiv>
-                            <input type=" radio" name="private[]" value="0" checked>公開
-                            <input type="radio" name="private[]" value="1">不公開<br>
-                            <input type="radio" name="continuous[]" value="0" checked>無持續
-                            <input type="radio" name="continuous[]" value="1">有持續
-                        </div>
-                    </div>`
-        $(".options").append(opt);
+    let optionIndex = 1;
+
+    function addOption(el) {
+        $(".lastInput").remove();
+        let opt = `<tr class=tdInput style='background-color: rgb(210, 222,239);height: 40px;border-bottom: 1px solid dodgerblue; '>
+            <th style="width: 5%;"></th>
+            <td><input type="date" name="eff_time[]" id="eff_time"></td>
+            <td><input type="text" name="project[]" id="project"></td>
+            <td><input type="text" name="details[]" id="details"></td>
+            <td><input type="text" name="category[]" id="category"></td>
+            <td><input type="number" name="amount[]" id="amount" min="0"></td>
+            <td><input type="number" name="price[]" id="price" min="0"></td>
+            <td>
+                <input type="radio" name="private[${optionIndex}][]" value="0" checked>公開
+                <input type="radio" name="private[${optionIndex}][]" value="1">不公開<br>
+                <input type="radio" name="continuous[${optionIndex}][]" value="0" checked>無持續
+                <input type="radio" name="continuous[${optionIndex}][]" value="1">有持續
+            </td>
+            <td>
+                <span onclick="addOption()" style="font-size: 26px;">＋</span><br>
+                <span onclick="removeOption(this) " style="font-size: 26px;">－</span>
+            </td>
+            <th style="width: 5%;"></th>
+            </tr>
+            <tr class=lastInput>
+            <th style="width: 5%;" colspan="10"></th>
+            </tr>`
+        $(".tdInput").parent().append(opt);
+        
+        optionIndex++; // 每次新增時增加索引值
     }
 
     function removeOption(el) {
-        let dom = $(el).parent()
+        let dom = $(el).parent().parent()
         $(dom).remove();
     }
 </script>
