b37d9e46ec2812c2cd2c5a940564f9c0ec32f9ab
Cerry0524 <Cerry0524@gmail.com>
2023-06-03 07:02:56 +0800

    修正登入畫面function功能

--- api/login.php
+++ api/login.php
@@ -1,17 +1,17 @@
 <?php include_once "../db.php";
 
-$sql="select count(*) from `members` where `acc`='{$_POST['acc']}' && `pw`='{$_POST['pw']}'";
-$chk=$pdo->query($sql)->fetchColumn();
-// _count('members',[
-//     'acc' => $_POST['acc'],
-//     'pw' => $_POST['pw']
-// ]);
+// $sql="select count(*) from `members` where `acc`='{$_POST['acc']}' && `pw`='{$_POST['pw']}'";
+// $chk=$pdo->query($sql)->fetchColumn();
+$chk=_count('members',[
+    'acc' => $_POST['acc'],
+    'pw' => $_POST['pw']
+]);
 
 if($chk){
     $_SESSION['login']=$_POST['acc'];
 
     if(isset($_SESSION['postion'])){
-        header("location:".$_SESSION['position']);
+        to($_SESSION['position']);
         unset($_SESSION['postion']);
         exit();
     };
